1425d466ace570bb7797ca17b34ad281a1a703d4
TherezaST <thereza.santos.adv@gmail.com>
2023-10-12 14:43:11 -0300

    abilities

---
+++ assets/js/convertPokemonToLi.1.js
@@ -0,0 +1,18 @@
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon ${pokemon.type}">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <span class="abilities">${pokemon.abilities}</span>
+
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+
+                <img src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+            </div>
+        </li>
+    `;
+}
